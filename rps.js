c64ca6cfa279c1cb08fe3a1bdfbdbd9b00139003
victorvang45 <victorvang456@gmail.com>
2023-10-20 17:06:55 -0500

    Added random generator for computer

--- rps.js
+++ rps.js
@@ -1,12 +1,25 @@
-console.log('Hello World');
-
-
 function getComputerChoice(){
-    
-
+    let computerChoice = '';
+    let computerNumChoice = Math.floor(Math.random() * 3);
+    if (computerNumChoice == 0) {
+        computerChoice = 'rock';
+    }
+    else if (computerNumChoice == 1){
+        computerChoice = 'paper';
+    } 
+    else if (computerNumChoice == 2) {
+        computerChoice = 'scissors';
+    } 
+    //console.log(computerNumChoice);
+    return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection){
+
+}
+
+
+function game(){
 
 }
 
