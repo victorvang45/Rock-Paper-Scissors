a1d5f7d855e71161bdfebeed3baa0b39d04eda9a
victorvang45 <victorvang456@gmail.com>
2023-10-20 20:07:51 -0500

    Added game logic to play 1 round of rock paper scissors

--- rps.js
+++ rps.js
@@ -1,26 +1,64 @@
-function getComputerChoice(){
-    let computerChoice = '';
+let playerScore = 0;
+let  computerScore = 0;
+
+
+function getComputerChoice(computerChoice)
+{
     let computerNumChoice = Math.floor(Math.random() * 3);
-    if (computerNumChoice == 0) {
+    if (computerNumChoice == 0) 
+    {
         computerChoice = 'rock';
     }
-    else if (computerNumChoice == 1){
+    else if (computerNumChoice == 1)
+    {
         computerChoice = 'paper';
     } 
-    else if (computerNumChoice == 2) {
+    else if (computerNumChoice == 2) 
+    {
         computerChoice = 'scissors';
     } 
-    //console.log(computerNumChoice);
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection)
+{
+    //let playerChoice = prompt("rock, paper, scissors?");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == computerSelection) 
+    {
+        return 'Tie';
+    }
+    else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'rock')) 
+    {
+        playerScore++;
+        return 'player won';
+    }
+    else 
+    {
+        computerScore++;
+        return 'Computer won, you lose.'
+    }
 
 }
 
 
-function game(){
+function game()
+{
+let playerSelection = '';
+let computerSelection = '';
 
+for(let i = 0; i < 5; i++) 
+{
+    playerSelection = prompt('Rock, Paper, Scissors?');
+    computerSelection = getComputerChoice(computerSelection);
+    console.log('player: ' + playerSelection);
+    console.log('computer: ' + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    
+}
 }
 
-console.log(getComputerChoice());
+console.log(game());
+console.log('player: ' + playerScore);
+console.log('computer: ' + computerScore);
